62dada08a8b04e168dd2087847c25a183ba74af7
Birdland <bird.land.13.13.13@gmail.com>
2024-10-05 22:13:23 -0400

    testing3

--- monitor-stuff/pixelbox-functions.lua
+++ monitor-stuff/pixelbox-functions.lua
@@ -26,7 +26,24 @@ function rect_empty(x1,y1,xsz,ysz,color,box)
 end
 
 function test_monitor(box)
-	divy = math.floor(box.width/8)
-	divx = math.floor(box.height/2)
+	divx = math.floor(box.width/8)
+	divy = math.floor(box.height/2)
 	
 	rect_filled(0,0,divx,divy,colors.white,box)
+	rect_filled(divx,0,divx,divy,colors.orange,box)
+	rect_filled(divx*2,0,divx,divy,colors.magenta,box)
+	rect_filled(divx*3,0,divx,divy,colors.lightBlue,box)
+	rect_filled(divx*4,0,divx,divy,colors.yellow,box)
+	rect_filled(divx*5,0,divx,divy,colors.lime,box)
+	rect_filled(divx*6,0,divx,divy,colors.pink,box)
+	rect_filled(divx*7,0,divx,divy,colors.gray,box)
+	
+	rect_filled(0,divy,divx,divy,colors.lightGray,box)
+	rect_filled(divx,divy,divx,divy,colors.cyan,box)
+	rect_filled(divx*2,divy,divx,divy,colors.purple,box)
+	rect_filled(divx*3,divy,divx,divy,colors.blue,box)
+	rect_filled(divx*4,divy,divx,divy,colors.brown,box)
+	rect_filled(divx*5,divy,divx,divy,colors.green,box)
+	rect_filled(divx*6,divy,divx,divy,colors.red,box)
+	rect_filled(divx*7,divy,divx,divy,colors.black,box)
+end
