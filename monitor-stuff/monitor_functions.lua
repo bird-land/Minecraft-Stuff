e6fea654e9384785d7a601d69ee0466c494e57e0
Birdland <bird.land.13.13.13@gmail.com>
2024-10-05 23:02:09 -0400

    More changes.

--- monitor-stuff/monitor_functions.lua
+++ monitor-stuff/monitor_functions.lua
@@ -1,33 +1,24 @@
-function write_characters(s,monitor,speed)
-	for i =1, #s do
-		local c = s:sub(i,i)
-		monitor.write(c)
-		sleep(speed)
-	end
-end
-
 function reign(monitor,speed)
-	argument = [[Is this the Region, this the Soil, the Clime,
-Said then the lost Arch-Angel, this the seat
-That we must change for Heav'n, this mournful gloom
-For that celestial light? Be it so, since he
-Who now is Sovran can dispose and bid
-What shall be right: fardest from him is best
-Whom reason hath equald, force hath made supream
-Above his equals. Farewel happy Fields
-Where Joy for ever dwells: Hail horrours, hail
-Infernal world, and thou profoundest Hell
-Receive thy new Possessor: One who brings
-A mind not to be chang'd by Place or Time.
-The mind is its own place, and in it self
-Can make a Heav'n of Hell, a Hell of Heav'n.
-What matter where, if I be still the same,
-And what I should be, all but less then he
-Whom Thunder hath made greater? Here at least
-We shall be free; th' Almighty hath not built
-Here for his envy, will not drive us hence:
-Here we may reign secure, and in my choyce
-To reign is worth ambition though in Hell:
-Better to reign in Hell, then serve in Heav'n.]]
-	write_characters(argument,monitor,speed)
+	textUtils.slowPrint("Is this the Region, this the Soil, the Clime,", speed)
+	textUtils.slowPrint("Said then the lost Arch-Angel, this the seat", speed)
+	textUtils.slowPrint("That we must change for Heav'n, this mournful gloom", speed)
+	textUtils.slowPrint("For that celestial light? Be it so, since he", speed)
+	textUtils.slowPrint("Who now is Sovran can dispose and bid", speed)
+	textUtils.slowPrint("What shall be right: fardest from him is best", speed)
+	textUtils.slowPrint("Whom reason hath equald, force hath made supream", speed)
+	textUtils.slowPrint("Above his equals. Farewel happy Fields", speed)
+	textUtils.slowPrint("Where Joy for ever dwells: Hail horrours, hail", speed)
+	textUtils.slowPrint("Infernal world, and thou profoundest Hell", speed)
+	textUtils.slowPrint("Receive thy new Possessor: One who brings", speed)
+	textUtils.slowPrint("A mind not to be chang'd by Place or Time.", speed)
+	textUtils.slowPrint("The mind is its own place, and in it self", speed)
+	textUtils.slowPrint("Can make a Heav'n of Hell, a Hell of Heav'n.", speed)
+	textUtils.slowPrint("What matter where, if I be still the same,", speed)
+	textUtils.slowPrint("And what I should be, all but less then he", speed)
+	textUtils.slowPrint("Whom Thunder hath made greater? Here at least", speed)
+	textUtils.slowPrint("We shall be free; th' Almighty hath not built", speed)
+	textUtils.slowPrint("Here for his envy, will not drive us hence:", speed)
+	textUtils.slowPrint("Here we may reign secure, and in my choyce", speed)
+	textUtils.slowPrint("To reign is worth ambition though in Hell:", speed)
+	textUtils.slowPrint("Better to reign in Hell, then serve in Heav'n", speed)
 end
