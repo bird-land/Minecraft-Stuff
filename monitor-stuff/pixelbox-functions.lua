5a9819061b489cf4207883502e0c88c7836e73d7
Birdland <bird.land.13.13.13@gmail.com>
2024-10-05 22:05:45 -0400

    testing2

--- monitor-stuff/pixelbox-functions.lua
+++ monitor-stuff/pixelbox-functions.lua
@@ -1,19 +1,19 @@
 function horizontal_line(x1,xsz,y1,color,box)
 	for i = x1, (x1+xsz) do
-		box.canvas[i][y1] = color
+		box.canvas[y1][i] = color
 	end
 end
 		
 function vertical_line(x1,y1,ysz,color,box)
 	for j = y1, (y1+ysz) do
-		box.canvas[x1][j] = color
+		box.canvas[j][x1] = color
 	end
 end
 
 function rect_filled(x1,y1,xsz,ysz,color,box)
 	for i = x1, (x1+xsz) do
-		for j = y1, (y1,ysz) do
-			box.canvas[i][j] = color
+		for j = y1, (y1+ysz) do
+			box.canvas[j][i] = color
 		end
 	end
 end
@@ -25,4 +25,8 @@ function rect_empty(x1,y1,xsz,ysz,color,box)
 	vertical_line((x1+xsz),y1,ysz,color,box)
 end
 
+function test_monitor(box)
+	divy = math.floor(box.width/8)
+	divx = math.floor(box.height/2)
 	
+	rect_filled(0,0,divx,divy,colors.white,box)
